3d085bc7d62fb45b5dabf92636c578a99ad0027b
kadayogit <guledodowaa@gmail.com>
2023-03-27 20:26:07 +0300

    Added global and non global modules examples

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 const app = require('./app');
 
 console.log(app.test());
+
+//Global modules
+console.log("The console log function is global modules");
+console.log(__dirname);  // global modules don't need to import 
+console.log(__filename);
+
+// Non Global modules
+const fs = require('fs');
+fs.writeFileSync('api.txt',"name: Guled Mohamed, title: manager"); // you must import the fs modules
