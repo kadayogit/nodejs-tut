d63b2bab52f9e818f6f8f346e081c8a6f1b6aaff
kadayogit <guledodowaa@gmail.com>
2023-03-31 17:33:05 +0300

    Added create files with in folder and read it

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+// create files within folder and read them again
 const fs = require('fs');
+var path = require('path');
 
-// how to create input text dynamically 
-const input = process.argv;
+var dirPath = path.join(__dirname, 'files');
+console.log(dirPath);
 
-// add or remove the file using if condition
-if(input[2] == 'add') {
-    fs.writeFileSync(input[3], input[4]);
+// for loop to files within folder
+for(i = 0; i< 3; i++) {
+
+    //first way for syntax you can write it
+    // fs.writeFileSync(dirPath+'file'+i+'.txt','guled file name');
+
+    //second way for syntax you can write it
+    fs.writeFileSync(`${dirPath}/file${i}.txt`,'guled file name');
 }
-else if (input[2] == 'remove'){
-    fs.unlinkSync(input[3])
-}
-else {
-    console.log("Invalid input text");
-}
+
+//how to read files with in folder and display
+fs.readdir(dirPath, (err, files)=>{
+console.log(files);
+// also you can use foreach loop to display line by line the files
+files.forEach((item)=>{
+    console.log(item);
+})
+});
